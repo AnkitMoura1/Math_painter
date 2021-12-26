ae738c31b53768dc7eae8bc0434d59e72bd2e167
Ankit <ankit2moura@gmail.com>
2021-12-26 13:50:27 +0530

    implementing make method in canvas class

--- canvas.py
+++ canvas.py
@@ -4,11 +4,13 @@ from PIL import Image
 
 class Canvas:
 
-    def __init__(self, width, height, color):
+    def __init__(self, height, width, color):
         self.width = width
         self.height = height
         self.color = color
+        self.data = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        self.data[:] = self.color
 
     def make(self, file_name):
-       pass
-
+        img = Image.fromarray(self.data, "RGB")
+        img.save(file_name)
